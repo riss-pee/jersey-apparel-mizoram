2c95516c9cecec1b0626ade5c308224e8d8f163f
riss-pee <rspacck@gmail.com>
2026-02-07 23:31:35 +0530

    Update delivery information in Home component

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -184,8 +184,8 @@ const Home: React.FC<HomeProps> = ({ products, reviews, heroSlides, onAddToCart,
             </div>
             <div className="space-y-4">
               <div className="w-12 h-12 bg-jam-green/10 rounded-2xl flex items-center justify-center text-jam-green mx-auto"><svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2.5" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"/><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2.5" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"/></svg></div>
-              <h4 className="text-xs font-black uppercase tracking-widest">Aizawl Delivery</h4>
-              <p className="text-[10px] text-gray-500 font-bold leading-relaxed px-8 uppercase">Fastest local shipping within 24 hours.</p>
+              <h4 className="text-xs font-black uppercase tracking-widest">Lunglei Delivery</h4>
+              <p className="text-[10px] text-gray-500 font-bold leading-relaxed px-8 uppercase">Lunglei khawchhunga : Free Delivery</p>
             </div>
             <div className="space-y-4">
               <div className="w-12 h-12 bg-jam-green/10 rounded-2xl flex items-center justify-center text-jam-green mx-auto"><svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2.5" d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"/></svg></div>
